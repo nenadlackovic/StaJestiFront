ee42dc2490630d1e8807304fac1eb04d5f55ef72
nenadlackovic <nenadlackovic13@gmail.com>
2022-04-28 10:31:12 +0200

    Kreiranje komponente GlavnaStrana

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GlavnaStranaComponent } from './glavna-strana/glavna-strana.component';
+import {RouterModule, Routes} from "@angular/router";
+
+
+const appRoute: Routes = [
+  {path: 'GlavnaStrana', component:GlavnaStranaComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GlavnaStranaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
